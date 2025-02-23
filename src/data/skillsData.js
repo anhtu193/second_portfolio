f7ba632b64f8e9cb7b6c03b51ca9d1c440b0fcb9
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2025-02-23 12:33:34 +0700

    add skills page

---
+++ src/data/skillsData.js
@@ -0,0 +1,48 @@
+const skills = {
+	1: [
+		{
+			title: "HTML",
+			level: 6,
+		},
+		{
+			title: "CSS",
+			level: 4,
+		},
+		{
+			title: "SASS/SCSS",
+			level: 2,
+		},
+		{
+			title: "JavaScript",
+			level: 4,
+		},
+		{
+			title: "TypeScript",
+			level: 3,
+		},
+	],
+	2: [
+		{
+			title: "React",
+			level: 6,
+		},
+		{
+			title: "Next.js",
+			level: 5,
+		},
+		{
+			title: "TailwindCSS",
+			level: 4,
+		},
+		{
+			title: "shadcn/ui",
+			level: 6,
+		},
+		{
+			title: "Material UI",
+			level: 3,
+		},
+	],
+};
+
+export default skills;
